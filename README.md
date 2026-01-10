3bf904213c0e4c42c5ee7043281d0cdeb0cbfbd8
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-01-10 01:25:48 +0100

    feat: add '2. Pixel Veil' animation with docs

--- README.md
+++ README.md
@@ -19,3 +19,13 @@ I found this effect in a YouTube video where someone was reviewing front-end dev
 <div align="center">
     <img src="assets/photo-bubble.gif" alt="Photo Bubble"/>
 </div>
+
+### 3. Pixel Veil
+
+While browsing projects on GitHub, I opened one that was still building on render.com, so I had to wait. On the right side, there was a grid of empty squares that revealed pixel-like artwork on hover, and it immediately caught my attention. At first, I had a completely different idea for this animation - it was supposed to wrap around the page and cover only part of the text, but I didn’t like how it felt. The animation itself was interesting enough that I kept trying to reuse it in different ways, with one idea leading to another. I kept working on it from the afternoon until late at night, until it evolved into its current form.
+
+Hmmm… did I just admit to my procrastination?
+
+<div align="center">
+    <img src="assets/pixel-veil.gif" alt="Pixel Veil"/>
+</div>
--- script.js
+++ script.js
@@ -22,3 +22,44 @@ const addLetter = () => {
 
 setInterval(() => typingRevealCursorSpan.classList.toggle('active'), 400);
 addLetter();
+
+/* //////////////////////// */
+/* Pixel Veil */
+/* //////////////////////// */
+
+// Create Grid
+document.querySelectorAll('.pixel-veil-grid-row').forEach((row) => {
+  // 16 - pixel count
+  for (let i = 0; i < 16; i++) {
+    const gridPixel = document.createElement('div');
+    gridPixel.classList.add('pixel-veil-grid-pixel');
+
+    const isTransparent = Math.random() > 0.6;
+    if (isTransparent) {
+      gridPixel.classList.add('transparent');
+    } else {
+      gridPixel.classList.add('initial-fade');
+    }
+
+    row.appendChild(gridPixel);
+  }
+});
+
+// Grid Animation
+document.querySelector('.pixel-veil-grid-conatiner').addEventListener(
+  'mouseenter',
+  function (event) {
+    const target = event.target;
+
+    if (!target.classList.contains('pixel-veil-grid-pixel')) return;
+    if (target.classList.contains('transparent')) return;
+    if (target.classList.contains('revealing')) return;
+
+    target.classList.add('revealing');
+
+    setTimeout(() => {
+      target.classList.remove('revealing');
+    }, 600);
+  },
+  true,
+);
